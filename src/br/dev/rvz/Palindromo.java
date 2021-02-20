06efe8e3bdce9663b084b7f30c65e68bd9b36fad
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-02-20 10:50:27 -0300

    Criar método para validar se o numero é palindromo ou não

--- src/br/dev/rvz/Palindromo.java
+++ src/br/dev/rvz/Palindromo.java
@@ -1,6 +1,14 @@
 package br.dev.rvz;
 
 public class Palindromo {
+    public static boolean isPalindrome(int number) {
+        int revertido = numeroRevertido(number);
+        if (number == revertido) {
+            true;
+        }
+        return false;
+    }
+
     public static int numeroRevertido(int numero) {
         int revertido = 0;
         while(numero < 0) {
--- src/br/dev/rvz/SumDigit.java
+++ src/br/dev/rvz/SumDigit.java
@@ -16,4 +16,6 @@ public class SumDigit {
 
         return sum;
     }
+
+
 }
