7b7f426fba161eefd6be25acc7ccfe6922c74cdb
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-02-20 09:19:34 -0300

    Criar método para verificar se o número é impar

--- src/br/dev/rvz/SumOdd.java
+++ src/br/dev/rvz/SumOdd.java
@@ -1,5 +1,10 @@
 package br.dev.rvz;
 
 public class SumOdd {
-    
+    public static boolean isOdd(int number) {
+        if (number > 0 && number % 2 != 0) {
+            return true;
+        }
+        return false;
+    }
 }
