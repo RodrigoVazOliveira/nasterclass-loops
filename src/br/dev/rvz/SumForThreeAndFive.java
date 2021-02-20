173c54733ab049c25999d046354556ac3b9bda92
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-02-20 08:49:00 -0300

    incluido propriedades para somar os números e a lista para armazenar os número que não são divisiveis por 3 e 5

---
+++ src/br/dev/rvz/SumForThreeAndFive.java
@@ -0,0 +1,11 @@
+package br.dev.rvz;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SumForThreeAndFive {
+    private static int sum = 0;
+    private static List<Integer> numbers = new ArrayList<>();
+
+
+}
