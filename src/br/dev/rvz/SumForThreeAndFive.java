d1e6ced3112847d4a762344c9072626c2d393ba8
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-02-20 08:55:04 -0300

    Documentar classe SumForThreeAndFive

--- src/br/dev/rvz/SumForThreeAndFive.java
+++ src/br/dev/rvz/SumForThreeAndFive.java
@@ -3,6 +3,14 @@ package br.dev.rvz;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Responsável por calcular a soma dos números
+ * divisíveis por 3 e 5 entre 1 a 1000
+ * e armazenar os números não divisíveis entre 1 a 1000
+ * por 3 e 5.
+ * @author Rodrigo Vaz
+ * @version 0.0.0
+ * */
 public class SumForThreeAndFive {
     private static int sum = 0;
     private static List<Integer> numbers = new ArrayList<>();
