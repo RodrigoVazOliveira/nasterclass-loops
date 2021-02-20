24ad63b7ac29bb35887633f625f9926c18d1cc25
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-02-20 08:52:41 -0300

    Criar testes da função na application

--- src/br/dev/rvz/Application.java
+++ src/br/dev/rvz/Application.java
@@ -2,6 +2,7 @@ package br.dev.rvz;
 
 public class Application {
     public static void main(String[] args) {
-        
+        System.out.println("A soma dos números divisíveis por 3 e 5 é igual a " + SumForThreeAndFive.sumNumbers());
+        System.out.println("Os números não divisíveis por 3 e 5 são: " + SumForThreeAndFive.getNumbers());
     }
 }
