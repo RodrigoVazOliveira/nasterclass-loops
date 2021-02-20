389f16084dbe3a92671097498935c1e8310481a7
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-02-20 10:54:56 -0300

    retirei a soma dupla no número invertiido, resolvendo o bug da soma

--- src/br/dev/rvz/Palindromo.java
+++ src/br/dev/rvz/Palindromo.java
@@ -11,8 +11,8 @@ public class Palindromo {
 
     public static int numeroRevertido(int numero) {
         int revertido = 0;
-        while(numero < 0) {
-            revertido += (revertido * 10) + (numero % 10);
+        while(numero > 0) {
+            revertido = (revertido * 10) + (numero % 10);
             numero /= 10;
         }
         return revertido;
