a253fa86707e3866106fab738bf3da8b299a0288
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-02-20 10:49:04 -0300

    Criar método para reverter o número

--- src/br/dev/rvz/Palindromo.java
+++ src/br/dev/rvz/Palindromo.java
@@ -1,4 +1,12 @@
 package br.dev.rvz;
 
 public class Palindromo {
+    public static int numeroRevertido(int numero) {
+        int revertido = 0;
+        while(numero < 0) {
+            revertido += (revertido * 10) + (numero % 10);
+            numero /= 10;
+        }
+        return revertido;
+    }
 }
