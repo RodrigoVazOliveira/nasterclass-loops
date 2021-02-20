39c4b1d386357e8585058bb28514816f39f65680
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-02-20 11:27:10 -0300

    Criar metodo somar primeiro e último digito

--- src/br/dev/rvz/SumDigit.java
+++ src/br/dev/rvz/SumDigit.java
@@ -17,5 +17,26 @@ public class SumDigit {
         return sum;
     }
 
+    public static int sumFirstAndLastDigit(int number) {
+        int firstDigit = 0, lastDigit = 0;
+        if (number < 0) {
+            return -1;
+        }
+
+        if (number >= 0 && number <= 9) {
+            lastDigit = number;
+        } else {
+            lastDigit = number % 10;
+        }
+
+        while (number > 0) {
+            if (number >= 0 && number <= 9) {
+                firstDigit = number;
+            }
+            number /= 10;
+        }
+        return firstDigit + lastDigit;
+    }
+
 
 }
