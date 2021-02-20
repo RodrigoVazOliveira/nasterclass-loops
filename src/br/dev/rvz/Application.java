c69556c200eb98b0c1dbde3524238283780b2c5f
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-02-20 09:35:30 -0300

    Criar teste de soma de números impares

--- src/br/dev/rvz/Application.java
+++ src/br/dev/rvz/Application.java
@@ -4,5 +4,13 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("A soma dos números divisíveis por 3 e 5 é igual a " + SumForThreeAndFive.sumNumbers());
         System.out.println("Os números não divisíveis por 3 e 5 são: " + SumForThreeAndFive.getNumbers());
+
+        System.out.println("Soma dos números impares entre 1 e 1000 " + SumOdd.sumOdd(1,1000));
+        System.out.println("Soma dos números impares entre -1 e 1000 " + SumOdd.sumOdd(-1,1000));
+        System.out.println("Soma dos números impares entre 100 e 100 " + SumOdd.sumOdd(100,100));
+        System.out.println("Soma dos números impares entre 13 e 13 " + SumOdd.sumOdd(13,13));
+        System.out.println("Soma dos números impares entre 100 e -100 " + SumOdd.sumOdd(100,-100));
+        System.out.println("Soma dos números impares entre 100 e 1000 " + SumOdd.sumOdd(100,1000));
     }
 }
+
