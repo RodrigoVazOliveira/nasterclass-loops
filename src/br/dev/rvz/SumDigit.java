0d42a0f86f60c700e14def569ff9c0e57ef17d53
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-02-20 10:33:21 -0300

    Criar metodo de soma para somar os digitos

--- src/br/dev/rvz/SumDigit.java
+++ src/br/dev/rvz/SumDigit.java
@@ -1,4 +1,19 @@
 package br.dev.rvz;
 
 public class SumDigit {
+    public static int sum(int number) {
+
+        int sum = 0;
+
+        if (number < 10) {
+            return -1;
+        }
+
+        while (number > 10) {
+            sum += number % 10;
+            number /= 10;
+        }
+
+        return sum;
+    }
 }
