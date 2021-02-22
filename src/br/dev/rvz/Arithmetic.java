0eb3b10dc50c33312a55696334e963569876998c
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-02-22 10:34:49 -0300

    Criar metodo getGreatestCommonDivisor

--- src/br/dev/rvz/Arithmetic.java
+++ src/br/dev/rvz/Arithmetic.java
@@ -1,5 +1,15 @@
 package br.dev.rvz;
 
-public class Arithmetic {]]
-    
+public class Arithmetic {
+    public static int getGreatestCommonDivisor(int firstNumber, int secondkNumber) {
+        if (firstNumber < 10 || secondkNumber < 10) {
+            return -1;
+        }
+        while(secondkNumber != 0) {
+            int temp = secondkNumber;
+            secondkNumber = firstNumber % secondkNumber;
+            firstNumber = temp;
+        }
+        return firstNumber;
+    }
 }
