4d99f827ed8d093c628418ca74ad2cebfa9512a5
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-02-22 08:57:30 -0300

    meotod de digitos compartilhado implementado

--- src/br/dev/rvz/SharedDigit.java
+++ src/br/dev/rvz/SharedDigit.java
@@ -1,4 +1,23 @@
 package br.dev.rvz;
 
 public class SharedDigit {
+    public static boolean hasSharedDigit(int numberOne, int numberTwo) {
+        if (numberOne < 10 || numberOne > 99 || numberTwo < 10 || numberTwo > 99) {
+            return false;
+        }
+
+        while (numberOne > 0) {
+            int digitOne = numberOne % 10;
+            int secondNUmber = numberTwo;
+            while (secondNUmber > 0) {
+                int digitTwo = secondNUmber % 10;
+                if (digitOne == digitTwo) {
+                    return true;
+                }
+                secondNUmber /= 10;
+            }
+            numberOne /= 10;
+        }
+        return false;
+    }
 }
