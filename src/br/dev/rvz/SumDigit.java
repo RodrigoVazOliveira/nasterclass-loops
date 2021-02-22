ec6a5cc2f6873c8ae5a21ae2eee550cad6c4f70d
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-02-22 08:47:59 -0300

    Adicionado moeto para somar apenas digitos pares

--- src/br/dev/rvz/SumDigit.java
+++ src/br/dev/rvz/SumDigit.java
@@ -38,5 +38,24 @@ public class SumDigit {
         return firstDigit + lastDigit;
     }
 
+    public static int getEvenDigitSum(int number) {
+        int sum = 0;
+        if (number < 0) {
+            return -1;
+        }
+
+        while (number > 0) {
+            int digit = number % 10;
+
+            if (digit % 2 == 0) {
+                sum += digit;
+            }
+
+            number /= 10;
+        }
+
+        return sum;
+    }
+
 
 }
