43811f8a11311faee2d536e4ca7abcff7cbccb33
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-02-22 11:12:19 -0300

    ajustando metodo sem variavel temporaria

--- src/br/dev/rvz/Arithmetic.java
+++ src/br/dev/rvz/Arithmetic.java
@@ -5,11 +5,27 @@ public class Arithmetic {
         if (firstNumber < 10 || secondkNumber < 10) {
             return -1;
         }
-        while(secondkNumber != 0) {
-            int temp = secondkNumber;
-            secondkNumber = firstNumber % secondkNumber;
-            firstNumber = temp;
+        while(firstNumber != secondkNumber) {
+            if (firstNumber > secondkNumber) {
+                firstNumber = firstNumber - secondkNumber;
+            } else {
+                secondkNumber = secondkNumber - firstNumber;
+            }
         }
         return firstNumber;
     }
+
+    public static void printFactors(int number) {
+        if (number > 1) {
+            for( int i = 1; i <= number; i++) {
+                if (number % i == 0) {
+                    System.out.print(i + " ");
+                }
+            }
+            System.out.println("");
+        } else{
+            System.out.println("Invalid Value");
+        }
+    }
+
 }
